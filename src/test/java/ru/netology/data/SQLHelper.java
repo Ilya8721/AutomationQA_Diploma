5b943ea2619af62513e99b3dd6f6495f611f0f40
Ilya Bobrov <i.bobrov87@gmail.com>
2023-06-14 16:08:44 +0300

    test classes added

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -12,27 +12,5 @@ public class SQLHelper {
     private static QueryRunner runner = new QueryRunner();
     private SQLHelper() {
     }
-//
-//    private static Connection getConn() throws SQLException {
-//        return DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
-//    }
-//
-//    public static DataHelper.VerificationCode getVerificationCode(String name) {
-//        var idSQL = "SELECT id FROM users WHERE login = ?;";
-//        var codeSQL = "SELECT code FROM auth_codes WHERE user_id = ? ORDER BY created DESC LIMIT 1;";
-//        try (var conn = getConn()){
-//            var id = runner.query(conn, idSQL, new ScalarHandler<>(),name);
-//            var code = runner.query(conn, codeSQL, new ScalarHandler<String>(), id);
-//            return new DataHelper.VerificationCode(code);
-//        } catch (SQLException exception) {
-//            exception.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    @SneakyThrows
-//    public static void cleanBase() {
-//        var connection = getConn();
-//        runner.execute(connection, "TRUNCATE auth_codes");
-//    }
+
 }
--- src/test/java/ru/netology/tests/PurchaseTravelTest.java
+++ src/test/java/ru/netology/tests/PurchaseTravelTest.java
@@ -3,42 +3,5 @@ package ru.netology.tests;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PurchaseTravelTest {
-//    @BeforeEach
-//    void setUp() {
-//        SQLHelper.cleanBase();
-//    }
-//
-//
-//    @Test
-//    @DisplayName("Should log in to your personal account")
-//    @SneakyThrows
-//    void shouldLogInToYourPersonalAccount() {
-//        String name = DataHelper.getAuthInfo1().getLogin();
-//        open("http://localhost:9999");
-//        var loginPage = new CreditGate.java();
-//        var authInfo = DataHelper.getAuthInfo1();
-//        var verificationPage = loginPage.validLogin(authInfo);
-//        var verificationCode = SQLHelper.getVerificationCode(name);
-//        verificationPage.validVerify(verificationCode.getCode());
-//    }
-//
-//    @Test
-//    @DisplayName("Should throw out an error message")
-//    @SneakyThrows
-//    void shouldThrowOutAnErrorMessage() {
-//        String name = DataHelper.getAuthInfo1().getLogin();
-//
-//        for (int count = 1; count <= 4; count++) {
-//            open("http://localhost:9999");
-//            var loginPage = new CreditGate.java();
-//            var authInfo = DataHelper.getAuthInfo1();
-//            var verificationPage = loginPage.validLogin(authInfo);
-//            var verificationCode = SQLHelper.getVerificationCode(name);
-//            if (count < 4) {
-//                verificationPage.validVerify(verificationCode.getCode());
-//            } else if (count == 4) {
-//                verificationPage.shouldReturnAnErrorMessage(verificationCode.getCode());
-//            }
-//        }
-//    }
+
 }
